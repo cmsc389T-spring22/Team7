48132f5ee79c860b342fc0e1c51c6de93ab9338e
asyfundyar <gulshierkhan@gmail.com>
2022-03-04 17:55:36 -0500

    method fixes

--- Projects/P3/src/Map.java
+++ Projects/P3/src/Map.java
@@ -60,7 +60,12 @@ public class Map {
 
 	public HashSet<Type> getLoc(Location loc) {
 		// wallSet and emptySet will help you write this method
-		return field.get(loc);
+		if (field.get(loc) != null) {
+			return field.get(loc);
+		} else {
+			return emptySet;
+		}
+
 	}
 
 	public boolean attack(String Name) {
