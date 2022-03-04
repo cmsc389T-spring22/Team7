6a7aa4fd630c5ec28f2d8336fa53082e78723a71
IvanaSanchezDiaz <46754901+IvanaSanchezDiaz@users.noreply.github.com>
2022-03-04 16:46:00 -0500

    Update TestMapAttack.java

--- Projects/P3/tests/TestMapAttack.java
+++ Projects/P3/tests/TestMapAttack.java
@@ -1,9 +1,41 @@
-import java.awt.Color;
 import junit.framework.*;
+import java.awt.Color;
+import java.io.*;
+import org.junit.Assert.*;
+import org.junit.Test;
 
-public class TestMapAttack {
+public class TestAttack extends TestCase {
 
-	public void testMapAttack() {
-		return null;	
+	@Test
+	public void testAttack1() {
+		
+		MainFrame frame = null;
+		
+		try {
+			frame = new MainFrame();
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		Ghost ghost = frame.addGhost(new Location(9, 11), "clyde", Color.orange);
+		PacMan pacman = frame.addPacMan(new Location(9, 12));
+		assertEquals(true, frame.getMap().attack("clyde"));
 	}
+	
+	@Test
+	public void testAttack2() {
+		
+		MainFrame frame = null;
+		
+		try {
+			frame = new MainFrame();
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		Ghost ghost = frame.addGhost(new Location(9, 8), "clyde", Color.orange);
+		PacMan pacman = frame.addPacMan(new Location(9, 12));
+		assertEquals(false, frame.getMap().attack("clyde"));
+	}
+	
 }
