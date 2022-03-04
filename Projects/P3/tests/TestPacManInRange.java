189ff1508cbcffb4c59cdddcc22f2bd3296e6ed2
ivanasanchezdiaz <ivana.sd@outlook.com>
2022-03-03 23:39:41 -0500

    TestPacManInRange

--- Projects/P3/tests/TestPacManInRange.java
+++ Projects/P3/tests/TestPacManInRange.java
@@ -1,10 +1,43 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import org.junit.Assert.*;
+import org.junit.Test;
 
 public class TestPacManInRange extends TestCase {
 
-	public void testPacManInRange() throws FileNotFoundException{
-		return null;
+	
+	@Test
+	public void testPacManInRange1() {
+		
+		MainFrame frame = null;
+		
+		try {
+			frame = new MainFrame();
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		Ghost ghost = frame.addGhost(new Location(9, 11), "name", Color.red);
+		PacMan pacman = frame.addPacMan(new Location(9, 12));
+		assertEquals(true, ghost.is_pacman_in_range());
 	}
+	
+	@Test
+	public void testPacManInRange2() {
+		
+		MainFrame frame = null;
+		
+		try {
+			frame = new MainFrame();
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		Ghost ghost = frame.addGhost(new Location(9, 8), "name", Color.red);
+		PacMan pacman = frame.addPacMan(new Location(9, 12));
+		assertEquals(false, ghost.is_pacman_in_range());
+	}
+	
+	
 }
