f46c237184178ec788ec4e3cc18f78d1e9575846
ivanasanchezdiaz <ivana.sd@outlook.com>
2022-03-04 01:26:01 -0500

    TestAttack

--- Projects/P3/tests/TestAttack.java
+++ Projects/P3/tests/TestAttack.java
@@ -1,10 +1,41 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import org.junit.Assert.*;
+import org.junit.Test;
 
 public class TestAttack extends TestCase {
 
-	public void testAttack() throws FileNotFoundException{
-		return null;	
+	@Test
+	public void testAttack1() {
+		
+		MainFrame frame = null;
+		
+		try {
+			frame = new MainFrame();
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		Ghost ghost = frame.addGhost(new Location(9, 11), "clyde", Color.orange);
+		PacMan pacman = frame.addPacMan(new Location(9, 12));
+		assertEquals(true, frame.getMap().attack("clyde"));
 	}
+	
+	@Test
+	public void testAttack2() {
+		
+		MainFrame frame = null;
+		
+		try {
+			frame = new MainFrame();
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		Ghost ghost = frame.addGhost(new Location(9, 8), "clyde", Color.orange);
+		PacMan pacman = frame.addPacMan(new Location(9, 12));
+		assertEquals(false, frame.getMap().attack("clyde"));
+	}
+	
 }
