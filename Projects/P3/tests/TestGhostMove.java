43a5f171b1eae902c4e848b487dda35762513af9
asyfundyar <gulshierkhan@gmail.com>
2022-03-03 22:34:28 -0500

    ghost-move tests added

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -2,9 +2,50 @@ import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class TestGhostMove extends TestCase {
 
-	public void testGhostMove() throws FileNotFoundException{
+	// this test should fail
 	
+	@Test
+	public void testGhostMoveFail() throws FileNotFoundException{
+		
+		// Creating A Map
+		MainFrame frame = null;
+
+		try {
+			frame = new MainFrame();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		// Creates a red ghost named "name" at location x,y
+		Ghost ghost = frame.addGhost(new Location(0, 1), "clyde", Color.orange); 
+		ghost.move();
+
+		assertEquals(new Location(1, 0), ghost.myLoc); // should fail
+	}
+	
+	// this test should pass
+
+	@Test
+	public void testGhostMovePass() throws FileNotFoundException{
+			
+		// Creating A Map
+		MainFrame frame = null;
+
+		try {
+			frame = new MainFrame();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		// Creates a red ghost named "name" at location x,y
+		Ghost ghost = frame.addGhost(new Location(0, 1), "clyde", Color.orange);
+		ghost.move();
+
+		assertNotEquals(new Location(1, 0), ghost.myLoc); // should pass
 	}
 }
