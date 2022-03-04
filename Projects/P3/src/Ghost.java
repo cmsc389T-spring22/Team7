83dddb8e78e1fd70517371582c07d9e672a4c944
Yoel Popovici <popoviciyoel@gmail.com>
2022-03-04 14:54:40 -0500

    ghost attack

--- Projects/P3/src/Ghost.java
+++ Projects/P3/src/Ghost.java
@@ -25,6 +25,9 @@ public class Ghost{
 	}
 
 	public boolean attack() {
-		return is_pacman_in_range();
+		if(is_pacman_in_range()){
+			return myMap.attack(this.myName);
+		}
+		return false;
 	}
 }
